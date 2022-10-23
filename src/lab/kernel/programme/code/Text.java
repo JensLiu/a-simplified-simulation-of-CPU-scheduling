4c1932f835ea9748a0af8ca10f29f4dfd89978dd
Jens Liu <jensliu29@outlook.de>
2022-10-23 11:24:27 +0800

    renew

--- src/lab/kernel/programme/code/Text.java
+++
@@ -1,8 +0,0 @@
-package lab.kernel.programme.code;
-
-import java.util.ArrayList;
-
-public class Text {
-    public ArrayList<String> instructions;
-
-}
--- src/lab/user/Test.java
+++
@@ -1,9 +0,0 @@
-package lab.user;
-
-import lab.kernel.Kernel;
-import lab.kernel.programme.Programme;
-
-public class Test {
-    Programme p = new Programme();
-    Kernel k = new Kernel();
-}
